a6ae0e45f086fcf0a2a28289ccae08188ab9bf83
AlexParpa <parpalea.alexandru@gmail.com>
2021-08-12 10:10:25 +0300

    added sign in page and form input

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.components';
 import Header from './components/header/header.component.jsx';
 import './App.css';
 
@@ -31,6 +32,7 @@ function App() {
         */}
         <Route exact path='/' component={HomePage}/>        
         <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignInAndSignUpPage}/>
 
       </Switch>
     </div>
